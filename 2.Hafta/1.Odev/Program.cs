7f6449b4d3747d9afa2db33deb9a8d890bcdd624
gokcaneren <gokcann01@gmail.com>
2022-03-28 12:37:17 +0300

    2.Hafta 1,Ödev

--- 2.Hafta/1.Odev/Program.cs
+++ 2.Hafta/1.Odev/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _1.Odev
 {
@@ -6,7 +8,27 @@ namespace _1.Odev
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            List<string> cities = new List<string>
+            {
+                "Ankara",
+                "İzmir",
+                "Adana",
+                "Eskişehir",
+                "İstanbul",
+                "Ankara",
+                "Eskişehir",
+                "Ankara",
+                "Ankara"
+            };
+
+            var citiesList = cities.Distinct();
+
+            foreach(var city in citiesList)
+            {
+                Console.WriteLine(city);
+            }
+
         }
     }
 }
