9bbdaf13e7311471aa5118d72323ec5e26054351
gokcaneren <gokcann01@gmail.com>
2022-03-30 22:09:39 +0300

    Simple OOP

---
+++ OOP/Class/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Person
+    {
+        public int id;
+        public string name;
+        public string lastName;
+
+
+
+        public void welcome(string name, string lastName)
+        {
+            Console.WriteLine($"Welcome {name} {lastName}");
+        }
+
+    }
+}
---
+++ OOP/Class/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Person();
+
+            person.id = 124;
+            person.name = "Gokcan";
+            person.lastName = "Eren";
+
+            person.welcome(person.name, person.lastName);
+
+        }
+    }
+}
