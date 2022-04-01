9d8dd2b52b81c6e6fb03d7a23074c06b3d0b10e0
gokcaneren <gokcann01@gmail.com>
2022-04-01 13:41:44 +0300

    Interface added

---
+++ OOP/Interface/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager();
+            manager.logger = new Firebase();
+            manager.Add();
+
+            
+        }
+    }
+
+    interface ILogger
+    {
+        void Log();
+    }
+
+    class Manager
+    {
+        public ILogger logger;
+        public void Add()
+        {
+            logger.Log();
+            Console.WriteLine("Customer added!");
+        }
+
+    }
+
+    class SqlDatabaseLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to Database!");
+        }
+    }
+
+    class MongoDb : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to MongoDb!");
+        }
+    }
+
+    class Firebase : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to Firebase");
+        }
+    }
+
+
+
+
+}
