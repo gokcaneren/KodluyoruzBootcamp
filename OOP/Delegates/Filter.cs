e178dd1f3288da415e26d9d38260cc8505eacd1b
gokcaneren <gokcann01@gmail.com>
2022-04-13 00:20:55 +0300

    Func Action practice

---
+++ OOP/Delegates/Filter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+
+    public static class Filters
+    {
+
+        //public delegate bool Criteria(int item);
+
+
+        public static List<int> Filter(List<int> values, Func<int, bool> criteria)
+        {
+            List<int> filtered = new List<int>();
+            foreach (var item in values)
+            {
+                if (criteria(item))
+                {
+                    filtered.Add(item);
+                }
+            }
+
+            return filtered;
+        }
+
+    }
+}
--- OOP/Delegates/Program.cs
+++ OOP/Delegates/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Delegates
 {
@@ -9,29 +11,55 @@ namespace Delegates
         public delegate void NumCarrier(int num);
         public delegate int Operator(int num1, int num2);
 
+
+
+        static List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
         static void Main(string[] args)
         {
-            //Main way of using Delegates
-            Messages message = new Messages();
-            FirstDelegate firstDelegate = message.HelloMessage;
-            firstDelegate();
-            firstDelegate += message.ByeByeMessage;
-            firstDelegate();
+            ////Main way of using Delegates
+            //Messages message = new Messages();
+            //FirstDelegate firstDelegate = message.HelloMessage;
+            //firstDelegate();
+            //firstDelegate += message.ByeByeMessage;
+            //firstDelegate();
 
 
-            MessagesToPerson toPerson = new MessagesToPerson();
-            MessageCarrier carrier = toPerson.MessageToWorker;
+            //MessagesToPerson toPerson = new MessagesToPerson();
+            //MessageCarrier carrier = toPerson.MessageToWorker;
 
-            carrier("Gokcan");
+            //carrier("Gokcan");
 
-            NumCarrier numCarrier = toPerson.TradePackage;
-            numCarrier(1000);
+            //NumCarrier numCarrier = toPerson.TradePackage;
+            //numCarrier(1000);
 
-            Operations operations = new Operations();
-            Operator operator1 = operations.SumOperations;
+            //Operations operations = new Operations();
+            //Operator operator1 = operations.SumOperations;
 
-            var result=operator1(3, 5);
-            Console.WriteLine(result);
+            //var result = operator1(3, 5);
+            //Console.WriteLine(result);
+
+
+            //var filtered = Filters.Filter(numbers, isEven);
+
+            //var filtered = Filters.Filter(numbers, delegate (int c) { return c < 5; });
+
+            var filtered = Filters.Filter(numbers, c => c % 2 == 0);
+
+            //foreach (var item in filtered)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            filtered.ForEach(p => Console.WriteLine(p));
+            filtered.Where(x => x % 5 == 0);
+        }
+
+        
+
+        static bool isEven(int item)
+        {
+            return item % 2 == 0;
         }
     }
 }
