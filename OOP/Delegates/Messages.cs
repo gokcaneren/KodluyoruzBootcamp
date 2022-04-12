db88b2aa52640f317a50f2ba3c139d175191833a
gokcaneren <gokcann01@gmail.com>
2022-04-12 13:30:07 +0300

    Into the Delegates!

---
+++ OOP/Delegates/Messages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class Messages
+    {
+        public void HelloMessage()
+        {
+            Console.WriteLine("Hello!");
+        }
+
+        public void ByeByeMessage()
+        {
+            Console.WriteLine("GoodBye!");
+        }
+    }
+}
---
+++ OOP/Delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        public delegate void FirstDelegate();
+
+        static void Main(string[] args)
+        {
+            Messages message = new Messages();
+
+            FirstDelegate firstDelegate = message.HelloMessage;
+            firstDelegate();
+
+            firstDelegate += message.ByeByeMessage;
+
+            firstDelegate();
+
+
+        }
+    }
+}
